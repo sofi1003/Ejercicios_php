60f0be560a0f5cbd975e1c7d19374f6396f7c03e
sofi1003 <sofia11.rodriguez@gmil.com>
2022-07-12 08:55:22 -0500

    Ejercicios Práctica PHP

---
+++ practica_php.php
@@ -0,0 +1,29 @@
+<?php
+    $saltoLinea = '<br>';
+    echo 'HOLA SOFI<br>';
+    echo "abcde" , "fghij<br>"; 
+    print "mi nombre es Sofi<br>";
+    
+    //Manejo de Variables
+    //Strings
+    $nombre = "Ana Sofia<br>";
+    echo $nombre;
+
+    $color = "verde<br>";
+    $COLOR = "rosa<br> ";
+
+    echo $color;
+    echo $COLOR;
+
+    //Operadores aritmeticos
+    $n = 5;
+    echo $n;
+    echo $nn = 2.6;
+    echo $nnn = true;
+    echo $saltoLinea;
+
+    //Varibales Globales o Locales
+
+    define("codigo", 123456);
+    echo codigo;
+?>
